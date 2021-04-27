9e9d46e7952e038688d9bffff58d5aecc3bd202b
Pritam Singh <pkspritam16@gmail.com>
2021-04-27 17:48:11 +0530

    add 01 - setup for chaincode dev
    Signed-off-by: Pritam Singh <pkspritam16@gmail.com>

---
+++ contract/contract.go
@@ -0,0 +1,38 @@
+package contract
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/hyperledger/fabric-protos-go/peer"
+)
+
+var (
+	// list of supported methods by invoke function
+	// mapping of function name with actual function
+	supportedMethods = map[string]func(stub shim.ChaincodeStubInterface, args []string) peer.Response{}
+)
+
+// Chaincode : implements shim.Chaincode interface
+type Chaincode struct{}
+
+// Init : invoke method called with -I (--isInit) flag.
+// use for initializing its initial state
+func (cc *Chaincode) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	return shim.Success(nil)
+}
+
+// Invoke : method called for updating and query state
+func (cc *Chaincode) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+	// get called method name along with arguments
+	methodName, args := stub.GetFunctionAndParameters()
+
+	// get method implantation for `methodName` from supported method mapping
+	method, ok := supportedMethods[methodName]
+	// check if method is supported or not
+	if !ok {
+		return shim.Error(fmt.Sprintf("method %s is not supported", methodName))
+	}
+	// call the method function
+	return method(stub, args)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Zzocker/cc-dev
+
+go 1.16
+
+require (
+	github.com/hyperledger/fabric-chaincode-go v0.0.0-20210319203922-6b661064d4d9
+	github.com/hyperledger/fabric-protos-go v0.0.0-20190919234611-2a87503ac7c9
+)
